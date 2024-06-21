434e5f50cfac16dea4f2a97b25b598380dfebd1d
Josevan Oliveira <josivantarcio@gmail.com>
2024-06-21 04:49:18 -0300

    Update CustomerRepository.java
    add comment

--- src/main/java/br/com/itau/itaubank/application/ports/out/CustomerRepository.java
+++ src/main/java/br/com/itau/itaubank/application/ports/out/CustomerRepository.java
@@ -1,18 +1,108 @@
-package br.com.itau.itaubank.application.ports.out;
-
-import br.com.itau.itaubank.domain.model.Customer;
+package com.unla.ghsicilianotfi.services.implementation;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface CustomerRepository {
-    Customer create(Customer customer);
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
-    Optional<Customer> findById(Long id);
+import com.unla.ghsicilianotfi.entities.Person;
+import com.unla.ghsicilianotfi.dtos.PersonDTO;
+import com.unla.ghsicilianotfi.repositories.IPersonRepository;
+import com.unla.ghsicilianotfi.services.IPersonService;
 
-    List<Customer> findAll();
+/**
+ * Serviço de implementação para gerenciar operações relacionadas a entidades Person.
+ */
+@Service("personService")
+public class PersonService implements IPersonService {
 
-    Customer update(Long id ,Customer customer);
+    private final IPersonRepository personRepository;
+    private final ModelMapper modelMapper = new ModelMapper();
 
-    void delete(Long id);
+    /**
+     * Construtor da classe PersonService.
+     *
+     * @param personRepository Repositório para acesso aos dados de Person.
+     */
+    public PersonService(IPersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    /**
+     * Recupera todas as entidades Person do repositório.
+     *
+     * @return Lista de todas as entidades Person.
+     */
+    @Override
+    public List<Person> getAll() {
+        return personRepository.findAll();
+    }
+
+    /**
+     * Insere ou atualiza uma entidade Person no repositório.
+     *
+     * @param person Entidade Person a ser inserida ou atualizada.
+     * @return Entidade Person inserida ou atualizada.
+     */
+    @Override
+    public Person insertOrUpdate(Person person) {
+        return personRepository.save(person);
+    }
+
+    /**
+     * Remove uma entidade Person do repositório pelo seu ID.
+     *
+     * @param id ID da entidade Person a ser removida.
+     * @return true se a entidade foi removida com sucesso, false caso contrário.
+     */
+    @Override
+    public boolean remove(int id) {
+        try {
+            personRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Procura uma entidade Person pelo seu ID.
+     *
+     * @param id ID da entidade Person.
+     * @return Optional contendo a entidade Person, se encontrada.
+     * @throws Exception Se ocorrer um erro durante a busca.
+     */
+    @Override
+    public Optional<Person> findById(int id) throws Exception {
+        return personRepository.findById(id);
+    }
+
+    /**
+     * Procura uma entidade Person pelo seu nome.
+     *
+     * @param name Nome da entidade Person.
+     * @return A entidade Person encontrada.
+     * @throws Exception Se não existir uma entidade Person com o nome fornecido.
+     */
+    @Override
+    public Person findByName(String name) throws Exception {
+        return personRepository.findByName(name)
+                .orElseThrow(() -> new Exception("ERROR: Não existe Pessoa com o nome: " + name));
+    }
+
+    /**
+     * Procura entidades Person pelo nome do grau acadêmico.
+     *
+     * @param degreeName Nome do grau acadêmico.
+     * @return Lista de PersonDTOs correspondente às entidades encontradas.
+     */
+    @Override
+    public List<PersonDTO> findByDegreeName(String degreeName) {
+        return personRepository.findByDegreeName(degreeName)
+                .stream()
+                .map(person -> modelMapper.map(person, PersonDTO.class))
+                .collect(Collectors.toList());
+    }
 }
